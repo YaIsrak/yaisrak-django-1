890aa11641af9b846baa4e21b9ab68ec87d35500
YaIsrak <israkdreamstar@gmail.com>
2021-10-29 15:45:08 +0600

    ok

--- Israksite/urls.py
+++ Israksite/urls.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 from django.views.static import serve
 from django.conf.urls import url
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Home.urls")),
@@ -13,4 +16,4 @@ urlpatterns = [
     #     {'document_root': settings.MEDIA_ROOT}),
     # url(r'^static/(?P<path>.*)$', serve,
     #     {'document_root': settings.STATIC_ROOT}),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
