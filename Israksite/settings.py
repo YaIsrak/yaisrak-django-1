ef700393ede96292ef009891513ccd5200f1549c
YaIsrak <israkdreamstar@gmail.com>
2021-10-29 16:39:53 +0600

    ok

--- Israksite/settings.py
+++ Israksite/settings.py
@@ -41,7 +41,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            # BASE_DIR / 'templates'
             os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
