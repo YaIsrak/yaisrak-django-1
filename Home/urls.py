2fa8ace78d5435b9e26ab22923b85ac7aef819f2
YaIsrak <israkdreamstar@gmail.com>
2021-10-29 16:49:12 +0600

    ok

--- Home/urls.py
+++ Home/urls.py
@@ -6,5 +6,4 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('pictures/', views.pictures),
-    # path('admin/', admin.site.url)
 ]
--- Israksite/urls.py
+++ Israksite/urls.py
@@ -11,9 +11,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Home.urls")),
-
-    # url(r'^media/(?P<path>.*)$', serve,
-    #     {'document_root': settings.MEDIA_ROOT}),
-    # url(r'^static/(?P<path>.*)$', serve,
-    #     {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
